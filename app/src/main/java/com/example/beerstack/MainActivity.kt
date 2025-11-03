b35aa7e554e0e38693caea74554b90aa5b01f85c
vande <noah.chang.vandewalle@student.howest.be>
2025-11-04 00:50:27 +0100

    Test Text Label Header

--- app/src/main/java/com/example/beerstack/MainActivity.kt
+++ app/src/main/java/com/example/beerstack/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.beerstack.ui.theme.BeerStackTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,10 +34,22 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun BeerStack(){
-    Header()
+    Header(modifier = Modifier
+        .fillMaxSize()
+    )
 }
 
 @Composable
-fun Header(){
-
+fun Header(modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text="test",
+            fontSize = 24.sp,
+            modifier = Modifier.padding(top = 24.dp)
+        )
+    }
 }
