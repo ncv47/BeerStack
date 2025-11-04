4b262875eb599e1644e43b8a1eca667c499d914e
vande <noah.chang.vandewalle@student.howest.be>
2025-11-04 12:30:37 +0100

    Added third page

---
+++ app/src/main/java/com/example/beerstack/ThirdActivity.kt
@@ -0,0 +1,12 @@
+package com.example.beerstack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+//Third Page For Login Page
+class ThirdActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
