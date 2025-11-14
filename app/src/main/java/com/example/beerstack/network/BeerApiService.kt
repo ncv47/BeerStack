f9f60ceadf878c28827e702c30d563120e65310d
vande <noah.chang.vandewalle@student.howest.be>
2025-11-14 19:54:00 +0100

    Import retrofit

--- app/src/main/java/com/example/beerstack/network/BeerApiService.kt
+++ app/src/main/java/com/example/beerstack/network/BeerApiService.kt
@@ -1,9 +1,16 @@
 package com.example.beerstack.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
 
 private const val BASE_URL = "https://api.untappd.com/v4"
 
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
 
-class BeerApiService {
+interface BeerApiService {
 
 }
