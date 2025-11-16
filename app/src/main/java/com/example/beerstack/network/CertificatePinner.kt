86c0c56a2b593b5b1d0baa865cce351a9d31c8b5
vande <noah.chang.vandewalle@student.howest.be>
2025-11-16 19:01:11 +0100

    WORKING SSL (HOLY DAMN)

--- app/src/main/java/com/example/beerstack/network/CertificatePinner.kt
+++ app/src/main/java/com/example/beerstack/network/CertificatePinner.kt
@@ -4,9 +4,21 @@ import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 
 val certificatePinner = CertificatePinner.Builder()
-    .add("api.sampleapis.com", "sha256/HsbawayQYhB8+cX46fHlLgTgcKsw9Vyb0BeRjJ2LVfY7E=")
+    .add(
+        "sampleapis.com",
+        "sha256/o2xLPMnEz7bPqBbMMLHftnkEDXcyLG2XnIamRz/aV3o="
+    )
+    .add(
+        "sampleapis.com",
+        "sha256/bzpISqORtwFMcNr2gKLqsOhuPqshIHjqx5Hk6g0Jg+E="
+    )
+    .add(
+        "sampleapis.com",
+        "sha256/hNSH3uUxjVFwgXPu7ogHgslkDHyIbZ4z8p2mxz1HNro="
+    )
     .build()
 
 val okHttpClient = OkHttpClient.Builder()
     .certificatePinner(certificatePinner)
+    //.certificatePinner(CertificatePinner.DEFAULT) //If you dont want to use SSL Pinning
     .build()
