2d64377d844a99a90f3aa5dabfaa8ff7e331f3bf
Boden Lancelot <lancelot.boden@student.howest.be>
2025-11-24 13:51:35 +0100

    BaseActivity test to close app on MainActivity

--- app/src/main/java/com/example/beerstack/BaseActivity.kt
+++ app/src/main/java/com/example/beerstack/BaseActivity.kt
@@ -17,40 +17,6 @@ abstract class BaseActivity : ComponentActivity() {
     }
 
     private fun isRooted(): Boolean {
-        return checkSuPath() || checkWhichSu()
-    }
-
-    private fun checkSuPath(): Boolean {
-        //Checks for su binaries
-        val paths = arrayOf(
-            "/sbin/su",
-            "/system/app/Superuser.apk",
-            "/system/bin/failsafe/su",
-            "/system/bin/su",
-            "/system/xbin/su",
-            "/system/sd/xbin/su",
-            "/data/local/xbin/su",
-            "/data/local/bin/su",
-            "/data/local/su"
-        )
-        return paths.any { java.io.File(it).exists() }
-    }
-
-    private fun checkWhichSu(): Boolean {
-        return try {
-            Runtime.getRuntime()
-                //Executes the command 'which su'
-                .exec(arrayOf("which", "su"))
-                //Read the output of the earlier executed command
-                .inputStream
-                .bufferedReader()
-                .readText()
-                //If it's not empty that means the device is likely rooted
-                //because 'which su' doesn't return anything if there isn't a super user on the device
-                .isNotEmpty()
-        } catch (_: Exception) {
-            //This catches any exceptions like if the command doesn't exist or shell execution is blocked
-            false
-        }
+        return true
     }
 }
