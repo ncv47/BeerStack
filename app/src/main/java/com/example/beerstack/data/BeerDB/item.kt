fdf4833c1e5bb27a0a50ab25ee50f17de211620b
Kenzo Haeck <kenzo.haeck@student.howest.be>
2025-11-27 10:41:06 +0100

    Eddited the item.kt file so the relation between userid and ownerid is possible

--- app/src/main/java/com/example/beerstack/data/BeerDB/item.kt
+++ app/src/main/java/com/example/beerstack/data/BeerDB/item.kt
@@ -1,8 +1,21 @@
 package com.example.beerstack.data.BeerDB
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Relation
+import com.example.beerstack.data.UserDB.User
 //werk
+
+data class UserwithBeer(
+    @Embedded val user: User,
+    @Relation(
+        parentColumn = "userid",
+        entityColumn = "ownerId"
+    )
+    val library: List<Item> // must be a list
+)
+
 @Entity(tableName = "items")
 data class Item(
     @PrimaryKey(autoGenerate = true)
@@ -11,5 +24,6 @@ data class Item(
     val beerprice: Int,
     val beerimage: String? = null,
     val beerrating: Int,
-    val beeraverage: Double = 0.0
+    val beeraverage: Double = 0.0,
+    val ownerId: Int
 )
