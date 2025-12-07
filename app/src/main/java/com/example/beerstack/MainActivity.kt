0adc5f1b19dd2cb860f65276bf81d850e5f9c398
Kenzo Haeck <kenzo.haeck@student.howest.be>
2025-12-07 17:59:00 +0100

    Removed unused import directive

--- app/src/main/java/com/example/beerstack/MainActivity.kt
+++ app/src/main/java/com/example/beerstack/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.beerstack.ui.theme.BeerStackTheme
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
