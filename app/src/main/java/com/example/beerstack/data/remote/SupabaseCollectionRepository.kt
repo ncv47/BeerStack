427479e5f08d5c96c99a9a45dd8826ee5fbb1af9
vande <noah.chang.vandewalle@student.howest.be>
2025-12-11 16:51:10 +0100

    API Service

---
+++ app/src/main/java/com/example/beerstack/data/remote/SupabaseCollectionRepository.kt
@@ -0,0 +1,27 @@
+package com.example.beerstack.data.remote
+
+import io.github.jan.supabase.postgrest.from
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class SupabaseCollectionRepository {
+
+    private val client = supabaseClient
+
+    suspend fun getCollection(userId: Int): List<UserBeerDto> =
+        withContext(Dispatchers.IO) {
+            client.from("BeerCollection")
+                .select {
+                    filter {
+                        UserBeerDto::userid eq userId
+                    }
+                }
+                .decodeList<UserBeerDto>()
+        }
+
+    suspend fun addBeerToCollection(item: UserBeerDto) =
+        withContext(Dispatchers.IO) {
+            client.from("BeerCollection")
+                .insert(item)
+        }
+}
