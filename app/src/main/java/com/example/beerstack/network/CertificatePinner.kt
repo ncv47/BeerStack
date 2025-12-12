77560ac9383a0361c39f48d4f8fbd8c736957f21
vande <noah.chang.vandewalle@student.howest.be>
2025-12-12 14:56:18 +0100

    New Own Beers API Fitted

--- app/src/main/java/com/example/beerstack/network/CertificatePinner.kt
+++ app/src/main/java/com/example/beerstack/network/CertificatePinner.kt
@@ -1,4 +1,4 @@
-package com.example.beerstack.network
+/*package com.example.beerstack.network
 
 import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
@@ -11,4 +11,4 @@ val okHttpClient: OkHttpClient = OkHttpClient.Builder()
     //.certificatePinner(certificatePinner) //remove commentary to use the certificatepinner with SSL
     //This is the 2nd method to SSL, primary method in res/xml/network_security_config.xml via AndroidManifest.xml
     .certificatePinner(CertificatePinner.DEFAULT) //If you dont want to use SSL Pinning (in this case this is skipped since we already have antoher working SSL method)
-    .build()
+    .build()*/
