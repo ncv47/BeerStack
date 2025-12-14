623fcb1c35a1520e7a1e9af17bda855c42d3a774
vande <noah.chang.vandewalle@student.howest.be>
2025-12-14 03:24:36 +0100

    Updated ANON KEY structure

--- app/src/main/java/com/example/beerstack/data/remote/SupabaseClientProvider.kt
+++ app/src/main/java/com/example/beerstack/data/remote/SupabaseClientProvider.kt
@@ -3,6 +3,16 @@ package com.example.beerstack.data.remote
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 
+//Stored unsafely
+//Normally ANON KEY should be in local properties and a link being made from build.gradle.kts like this
+/*
+buildConfigField(
+    "String",
+    "SUPABASE_ANON_KEY",
+    "\"${project.properties["SUPABASE_ANON_KEY"]}\""
+)
+ */
+//But local properties is different for everyone, so for the convenience of the project for school we leave them in plain text (not secure in a real scenario)
 private const val SUPABASE_URL = "https://dqkpzojnslcyeluzzyhi.supabase.co"
 private const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxa3B6b2puc2xjeWVsdXp6eWhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0NTYzODgsImV4cCI6MjA4MTAzMjM4OH0.wh3Id22_gXhKNzjIVbyZRUHnU4zhSayxXykxxQNS0WM"
 
