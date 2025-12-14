dc446263a963beb8202b4f3c4bc6fbea9e03f6bd
noah defruyt <noah.defruyt@student.howest.be>
2025-12-14 11:12:23 +0100

    added the gradient to the background of add your own beer

--- app/src/main/java/com/example/beerstack/SeventhActivity.kt
+++ app/src/main/java/com/example/beerstack/SeventhActivity.kt
@@ -2,17 +2,20 @@ package com.example.beerstack
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.example.beerstack.data.remote.SupabaseBeerDto
 import com.example.beerstack.data.remote.SupabaseCollectionRepository
+import com.example.beerstack.ui.theme.BeerGradient
 import com.example.beerstack.ui.theme.BeerStackTheme
 import kotlinx.coroutines.launch
 
@@ -26,7 +29,7 @@ class SeventhActivity : BaseActivity() {
         val beerRepo = SupabaseCollectionRepository()
 
         setContent {
-            BeerStackTheme {
+            BeerStackTheme (dynamicColor = false) {
                 AddOwnBeerScreen(
                     onSave = { dto ->
                         lifecycleScope.launch {
@@ -56,120 +59,126 @@ fun AddOwnBeerScreen(
     var imageUrl by remember { mutableStateOf("") }
     var selectedCurrency by remember { mutableStateOf("USD") }
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Text(
-            text = "Add your own beer",
-            style = MaterialTheme.typography.titleMedium,
+            .background(BeerGradient)
+    ){
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp)
-        )
-
-        OutlinedTextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Beer name") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp)
-        )
-
-        // De currency button
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            FilterChip(
-                selected = selectedCurrency == "USD",
-                onClick = { selectedCurrency = "USD" },
-                label = { Text("USD") }
+
+            Text(
+                text = "Add your own beer",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp)
             )
-            FilterChip(
-                selected = selectedCurrency == "EUR",
-                onClick = { selectedCurrency = "EUR" },
-                label = { Text("EUR") }
+
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Beer name") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
             )
-        }
 
-        //Format for the price
-        fun validatePriceInput(input: String): String {
-            val cleaned = input.replace(",", ".")
-            val regex = Regex("^\\d{0,7}(\\.\\d{0,2})?$") // max 7 cijfers voor de punt, 2 erna (1 MILLION BEERS)
-            return if (cleaned.isEmpty() || regex.matches(cleaned)) cleaned else priceText
-        }
-
-        OutlinedTextField(
-            value = priceText,
-            onValueChange = { new ->
-                priceText = validatePriceInput(new)
-            },
-            label = { Text("Price (0.00)") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp)
-        )
-
-        OutlinedTextField(
-            value = apiAverageText,
-            onValueChange = { apiAverageText = it },
-            label = { Text("API average") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp)
-        )
-
-        OutlinedTextField(
-            value = totalReviewsText,
-            onValueChange = { totalReviewsText = it },
-            label = { Text("Total reviews") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp)
-        )
-
-        OutlinedTextField(
-            value = imageUrl,
-            onValueChange = { imageUrl = it },
-            label = { Text("Stock image URL") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp)
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                val price = priceText.ifBlank { "0.00" }
-                val apiAvg = apiAverageText.toDoubleOrNull() ?: 0.0
-                val totalReviews = totalReviewsText.toIntOrNull() ?: 0
-
-                val dto = SupabaseBeerDto(
-                    id = null,            // API generates ID
-                    name = name,
-                    currency = selectedCurrency,
-                    price = price,
-                    apiaverage = apiAvg,
-                    reviews = totalReviews,
-                    imageurl = imageUrl
+            // De currency button
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                FilterChip(
+                    selected = selectedCurrency == "USD",
+                    onClick = { selectedCurrency = "USD" },
+                    label = { Text("USD") }
                 )
-                onSave(dto)
-            },
-            modifier = Modifier.fillMaxWidth(),
-            // This field has to be filled in for the button to work
-            enabled = name.isNotBlank()
-        ) {
-            Text("Save")
+                FilterChip(
+                    selected = selectedCurrency == "EUR",
+                    onClick = { selectedCurrency = "EUR" },
+                    label = { Text("EUR") }
+                )
+            }
+
+            //Format for the price
+            fun validatePriceInput(input: String): String {
+                val cleaned = input.replace(",", ".")
+                val regex = Regex("^\\d{0,7}(\\.\\d{0,2})?$") // max 7 cijfers voor de punt, 2 erna (1 MILLION BEERS)
+                return if (cleaned.isEmpty() || regex.matches(cleaned)) cleaned else priceText
+            }
+
+            OutlinedTextField(
+                value = priceText,
+                onValueChange = { new ->
+                    priceText = validatePriceInput(new)
+                },
+                label = { Text("Price (0.00)") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+
+            OutlinedTextField(
+                value = apiAverageText,
+                onValueChange = { apiAverageText = it },
+                label = { Text("API average") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+
+            OutlinedTextField(
+                value = totalReviewsText,
+                onValueChange = { totalReviewsText = it },
+                label = { Text("Total reviews") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+
+            OutlinedTextField(
+                value = imageUrl,
+                onValueChange = { imageUrl = it },
+                label = { Text("Stock image URL") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    val price = priceText.ifBlank { "0.00" }
+                    val apiAvg = apiAverageText.toDoubleOrNull() ?: 0.0
+                    val totalReviews = totalReviewsText.toIntOrNull() ?: 0
+
+                    val dto = SupabaseBeerDto(
+                        id = null,            // API generates ID
+                        name = name,
+                        currency = selectedCurrency,
+                        price = price,
+                        apiaverage = apiAvg,
+                        reviews = totalReviews,
+                        imageurl = imageUrl
+                    )
+                    onSave(dto)
+                },
+                modifier = Modifier.fillMaxWidth(),
+                // This field has to be filled in for the button to work
+                enabled = name.isNotBlank()
+            ) {
+                Text("Save")
+            }
         }
     }
 }
